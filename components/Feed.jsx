41082404ca2a9efd8625fd5027ab14edf0a81183
withabhinay <abhinaygiri08@gmail.com>
2024-09-24 22:15:27 +0530

    post error correction

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -8,7 +8,7 @@ const PromptCardList = ({ data, handleTagClick }) => {
     <div className='mt-16 prompt_layout'>
       {data.map((post) => (
         <PromptCard
-          key={post._id}
+          key={post.id}
           post={post}
           handleTagClick={handleTagClick}
         />
