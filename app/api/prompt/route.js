b0cbf50b6bb3aabd71d27fc5d8451ef2cf85d422
withabhinay <abhinaygiri08@gmail.com>
2024-09-28 09:59:26 +0530

    check

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,21 @@
 import { connectToDB } from "@utils/database";
 import Prompt from '@models/prompt';
 
-
 export const GET = async (request) => {
-    try {
-        await connectToDB();
-        const prompts = await Prompt.find({}).populate('creator');
-        return new Response(JSON.stringify(prompts), { status: 200 });
-    } catch (error) {
-        return new Response("Failed to fetch all prompts",{status:500})
-    }
-}
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate('creator');
+
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: {
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+        'Surrogate-Control': 'no-store',
+      },
+    });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -24,7 +24,12 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch("/api/prompt");
+    const response = await fetch("/api/prompt",{
+      method: "GET",
+      headers: {
+        "Cache-Control": "no-store", // Prevents caching
+      },
+    });
     const data = await response.json();
 
     setPosts(data);
